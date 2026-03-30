f4648888b05dc2897b6b02bc133e8fb6967796d6
TuongThiDuyen <dd16102004@gmail.com>
2026-03-30 22:33:47 +0700

    feat: project chefmate-automation
    - Thuc hien code

--- src/main/java/screens/FridgeScreen.java
+++ src/main/java/screens/FridgeScreen.java
@@ -1,15 +1,23 @@
-//package screens;
-//
-//import core.base.BaseScreen;
-//import org.openqa.selenium.By;
-//
-//import static core.utils.AssertionHelper.assertTrue;
-//
-//public class FridgeScreen extends BaseScreen {
-//    private final By fridgeScreen = By.xpath("//z0.h0/android.view.View/android.view.View/android.view.View[1]");
-//
-//    public void validateItemListVisible() {
-//        assertTrue(isDisplayed(fridgeScreen));
-//    }
-//}
-//
+package screens;
+
+import core.base.BaseScreen;
+import org.openqa.selenium.By;
+
+public class FridgeScreen extends BaseScreen {
+
+    private final By btnAddIngredient = By.xpath("//z0.h0/android.view.View/android.view.View/android.view.View[1]/android.view.View[2]/android.widget.Button");
+    private final By ingredientName = By.xpath("//android.widget.ScrollView/android.widget.EditText[1]");
+    private final By quantity = By.xpath("//android.widget.ScrollView/android.widget.EditText[2]");
+    private final By unit = By.xpath("//android.widget.ScrollView/android.widget.EditText[3]");
+    private final By expiryDate = By.xpath("//android.widget.ScrollView/android.widget.EditText[4]");
+    private final By btnSave = By.xpath("//android.widget.Button");
+
+    public void addIngredient(String name, String qty, String unitStr) {
+        logStep("Thêm nguyên liệu: " + name);
+        click(btnAddIngredient);
+        type(ingredientName, name);
+        type(quantity, qty);
+        type(unit, unitStr);
+        click(btnSave);
+    }
+}
