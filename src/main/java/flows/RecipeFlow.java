637d6f9b7edf8bb9c4fb7c9221fef98be5be8d6f
TuongThiDuyen <dd16102004@gmail.com>
2026-03-30 22:51:59 +0700

    feat: project chefmate-automation
    - Thuc hien Code Flow

--- src/main/java/flows/RecipeFlow.java
+++ src/main/java/flows/RecipeFlow.java
@@ -1,12 +1,25 @@
 package flows;
 
 import core.base.BaseFlow;
+import screens.HomeScreen;
 import screens.RecipeDetailScreen;
 
 public class RecipeFlow extends BaseFlow {
-    private final RecipeDetailScreen recipeDetailScreen = new RecipeDetailScreen();
 
-    public String getRecipeTitle() {
-        return recipeDetailScreen.getRecipeTitle();
+    private final HomeScreen home = new HomeScreen();
+    private final RecipeDetailScreen detail = new RecipeDetailScreen();
+
+    public void viewFirstRecipeDetail() {
+        logStep("Từ Home mở chi tiết công thức đầu tiên");
+        home.openFirstRecipe();
+    }
+
+    public void addCurrentRecipeToFavorite() {
+        logStep("Thêm công thức hiện tại vào yêu thích");
+        detail.addToFavorite();
+    }
+
+    public boolean isRecipeDetailDisplayed() {
+        return detail.isRecipeDetailDisplayed();
     }
 }
