d3e16b302385f399c2fd1f674eb459b8bbf46c12
TuongThiDuyen <dd16102004@gmail.com>
2026-03-30 22:42:06 +0700

    feat: project chefmate-automation
    - Thuc hien Code

--- src/main/java/screens/HomeScreen.java
+++ src/main/java/screens/HomeScreen.java
@@ -5,27 +5,14 @@ import org.openqa.selenium.By;
 
 public class HomeScreen extends BaseScreen {
 
-    // Header & Title
     private final By txtNauNgon = By.xpath("//android.widget.TextView[@text='Nấu ngon']");
-    private final By txtBepesTitle = By.xpath("//android.widget.TextView[@text='Bepes - Trợ lý bếp thông minh']");
-
-    // AI Chat & Gợi ý
     private final By btnBepesAI = By.xpath("//android.widget.TextView[@text='Trò chuyện với Bepes']");
     private final By btnGoiYTuTuLanh = By.xpath("//android.widget.TextView[@text='Gợi ý từ tủ lạnh']");
-
-    // Search
     private final By btnSearch = By.xpath("//android.widget.ImageView[@content-desc='Tìm kiếm']");
-
-    // Recipe cards
     private final By recipeCardFirst = By.xpath("//android.widget.ScrollView/android.view.View[1]");
-
-    // Bottom Navigation
     private final By bottomNavHome = By.xpath("//android.widget.TextView[@text='Trang chủ']");
     private final By bottomNavFridge = By.xpath("//android.widget.TextView[@text='Tủ lạnh']");
     private final By bottomNavProfile = By.xpath("//android.widget.TextView[@text='Tài khoản']");
-
-    // Icon chuông
-    private final By notificationBell = By.xpath("//z0.h0/android.view.View/android.view.View/android.view.View[1]/android.view.View[1]/android.widget.Button");
 
     public void openBepesAI() {
         logStep("Mở Trò chuyện với Bepes");
