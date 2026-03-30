8ca510e2837dddb2b33b0456481a1b9dcb22efeb
TuongThiDuyen <dd16102004@gmail.com>
2026-03-30 22:34:31 +0700

    feat: project chefmate-automation
    - Thuc hien code

--- src/main/java/screens/RecipeDetailScreen.java
+++ src/main/java/screens/RecipeDetailScreen.java
@@ -4,9 +4,17 @@ import core.base.BaseScreen;
 import org.openqa.selenium.By;
 
 public class RecipeDetailScreen extends BaseScreen {
-    private final By recipeTitle = By.id("recipe_title");
 
-    public String getRecipeTitle() {
-        return getText(recipeTitle);
+    private final By favoriteButton = By.xpath("//android.view.View[@content-desc='Like']");
+    private final By ingredientsTab = By.xpath("//android.widget.TextView[@text='Nguyên liệu']");
+    private final By stepsTab = By.xpath("//android.widget.TextView[@text='Cách thực hiện']");
+
+    public boolean isRecipeDetailDisplayed() {
+        return isDisplayed(favoriteButton);
+    }
+
+    public void addToFavorite() {
+        logStep("Thêm vào yêu thích");
+        click(favoriteButton);
     }
 }
