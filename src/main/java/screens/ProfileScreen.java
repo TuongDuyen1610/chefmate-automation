9a82c81eab23e398a6da118f904ebad1dc92c6f2
TuongThiDuyen <dd16102004@gmail.com>
2026-03-30 22:34:12 +0700

    feat: project chefmate-automation
    - Thuc hien code

--- src/main/java/screens/ProfileScreen.java
+++ src/main/java/screens/ProfileScreen.java
@@ -4,9 +4,22 @@ import core.base.BaseScreen;
 import org.openqa.selenium.By;
 
 public class ProfileScreen extends BaseScreen {
-    private final By usernameField = By.id("username_field");
 
-    public String getUsername() {
-        return getText(usernameField);
+    private final By txtUserName = By.xpath("//android.widget.TextView[@text='Tuong Thi Duyen']");
+    private final By btnEditProfile = By.xpath("//android.view.View[@content-desc='Edit']");
+    private final By btnLogout = By.xpath("//z0.h0/android.view.View/android.view.View/android.view.View[1]/android.widget.Button");
+
+    public boolean isProfileDisplayed() {
+        return isDisplayed(txtUserName);
+    }
+
+    public void clickEditProfile() {
+        logStep("Nhấn Chỉnh sửa hồ sơ");
+        click(btnEditProfile);
+    }
+
+    public void logout() {
+        logStep("Đăng xuất");
+        click(btnLogout);
     }
 }
