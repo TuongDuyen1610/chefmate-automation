fe4a4ab52088ae57e70ff28993dafe83e718c827
TuongThiDuyen <dd16102004@gmail.com>
2026-03-30 22:37:53 +0700

    feat: project chefmate-automation
    - Thuc hien Code

--- src/main/java/screens/EditProfileScreen.java
+++ src/main/java/screens/EditProfileScreen.java
@@ -1,4 +1,17 @@
 package screens;
 
-public class EditProfileScreen {
+import core.base.BaseScreen;
+import org.openqa.selenium.By;
+
+public class EditProfileScreen extends BaseScreen {
+
+    private final By fullNameField = By.xpath("//android.widget.EditText[@text='Tuong Thi Duyen']");
+    private final By emailField = By.xpath("//android.widget.EditText[@text='00000@gmail.com']");
+    private final By saveButton = By.xpath("//android.widget.TextView[@text='Lưu thay đổi']");
+
+    public void updateProfile(String newName, String newEmail) {
+        type(fullNameField, newName);
+        type(emailField, newEmail);
+        click(saveButton);
+    }
 }
