60d2899f3c816b75879bfc1721bff237ab633aac
TuongThiDuyen <dd16102004@gmail.com>
2026-03-30 22:37:34 +0700

    feat: project chefmate-automation
    - Thuc hien Code

--- src/main/java/screens/ShoppingListScreen.java
+++ src/main/java/screens/ShoppingListScreen.java
@@ -1,4 +1,20 @@
 package screens;
 
-public class ShoppingListScreen {
+import core.base.BaseScreen;
+import org.openqa.selenium.By;
+
+public class ShoppingListScreen extends BaseScreen {
+
+    private final By addManualBtn = By.xpath("//android.widget.TextView[@text='Thêm nguyên liệu thủ công']");
+    private final By ingredientNameField = By.xpath("//android.widget.ScrollView/android.widget.EditText[1]");
+    private final By completeBtn = By.xpath("//z0.h0/android.view.View/android.view.View[3]/android.widget.Button");
+
+    public void addManualIngredient(String name) {
+        click(addManualBtn);
+        type(ingredientNameField, name);
+    }
+
+    public void completeList() {
+        click(completeBtn);
+    }
 }
