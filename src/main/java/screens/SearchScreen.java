e47b1e141eadd69292bd2d234471b34ad85432b9
TuongThiDuyen <dd16102004@gmail.com>
2026-03-30 22:36:28 +0700

    feat: project chefmate-automation
    - Thuc hien Code
    SearchScreen.java

--- src/main/java/screens/SearchScreen.java
+++ src/main/java/screens/SearchScreen.java
@@ -1,4 +1,19 @@
 package screens;
 
-public class SearchScreen {
+import core.base.BaseScreen;
+import org.openqa.selenium.By;
+
+public class SearchScreen extends BaseScreen {
+
+    private final By searchEditText = By.xpath("//android.widget.EditText");
+    private final By resultItem = By.xpath("//android.widget.ScrollView/android.view.View[1]");
+
+    public void search(String keyword) {
+        logStep("Tìm kiếm: " + keyword);
+        type(searchEditText, keyword);
+    }
+
+    public void openFirstResult() {
+        click(resultItem);
+    }
 }
