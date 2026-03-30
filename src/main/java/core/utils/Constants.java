538e745e845402dec98f17ededd0d62c78645687
TuongThiDuyen <dd16102004@gmail.com>
2026-03-30 22:59:03 +0700

    feat: project chefmate-automation
    - Thuc hien Code Flow

--- src/main/java/core/utils/Constants.java
+++ src/main/java/core/utils/Constants.java
@@ -1,16 +1,16 @@
 package core.utils;
 
 public final class Constants {
-
+    // Test Data mặc định
     public static final String DEFAULT_PHONE = "0000000000";
     public static final String DEFAULT_PASSWORD = "000000";
     public static final String DEFAULT_EMAIL = "00000@gmail.com";
-
+    // Timeout
     public static final int EXPLICIT_WAIT = 15;
     public static final int IMPLICIT_WAIT = 10;
-
+    // Messages dùng chung
     public static final String LOGIN_SUCCESS = "Đăng nhập thành công";
     public static final String LOGIN_FAIL = "Sai thông tin đăng nhập";
 
-    private Constants() {}
+    private Constants() {} // Không cho tạo instance
 }
