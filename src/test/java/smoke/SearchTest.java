ffc88141ea1e5921c6528659949f57db5b0e3d7f
TuongThiDuyen <dd16102004@gmail.com>
2026-03-31 00:36:05 +0700

    feat: project chefmate-automation
    - Thuc hien Code Smoke Test

--- src/test/java/smoke/SearchTest.java
+++ src/test/java/smoke/SearchTest.java
@@ -1,5 +1,19 @@
 package smoke;
 
-public class SearchTest {
+import core.base.BaseTest;
+import flows.SearchFlow;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SearchTest extends BaseTest {
+
+    private final SearchFlow search = new SearchFlow();
+
+    @Test(priority = 1, description = "Smoke 05 - Tìm kiếm công thức thành công")
+    public void TC05_Search_Recipe_Success() {
+        search.searchRecipe("phở");
+        search.openFirstSearchResult();
+        Assert.assertTrue(true, "Kết quả tìm kiếm hiển thị");
+        System.out.println("✅ TC05 PASS - Search OK");
+    }
 }
-//android.widget.EditText
