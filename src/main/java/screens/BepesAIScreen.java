a9299610bc20725a0e4a459aeb8c42eeab9e50dd
TuongThiDuyen <dd16102004@gmail.com>
2026-03-30 22:33:22 +0700

    feat: project chefmate-automation
    - Thuc hien code

--- src/main/java/screens/BepesAIScreen.java
+++ src/main/java/screens/BepesAIScreen.java
@@ -4,9 +4,18 @@ import core.base.BaseScreen;
 import org.openqa.selenium.By;
 
 public class BepesAIScreen extends BaseScreen {
-    private final By aiChatBox = By.id("ai_chat_box");
 
-    public void typeInChat(String message) {
-        type(aiChatBox, message);
+    private final By chatInput = By.xpath("//android.widget.EditText");
+    private final By btnSend = By.xpath("//android.widget.EditText/android.view.View/android.widget.Button");
+    private final By aiResponse = By.xpath("//android.widget.TextView[contains(@text,'Bepes')]");
+
+    public void sendQuestion(String question) {
+        logStep("Gửi câu hỏi: " + question);
+        type(chatInput, question);
+        click(btnSend);
+    }
+
+    public boolean isAIResponseDisplayed() {
+        return isDisplayed(aiResponse);
     }
 }
