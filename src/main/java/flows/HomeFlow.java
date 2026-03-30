d8a0d82d00eaf0123e312f8fe6548d312c79fed4
TuongThiDuyen <dd16102004@gmail.com>
2026-03-30 22:50:14 +0700

    feat: project chefmate-automation
    - Thuc hien Code Flow

--- src/main/java/flows/HomeFlow.java
+++ src/main/java/flows/HomeFlow.java
@@ -8,15 +8,27 @@ public class HomeFlow extends BaseFlow {
     private final HomeScreen home = new HomeScreen();
 
     public void openAppAndGoToHome() {
-        logStep("Mở app và vào Home");
+        logStep("Mở app và xác nhận vào màn hình Home");
         home.isHomeDisplayed();
     }
 
     public void goToBepesAI() {
+        logStep("Từ Home → Mở Trò chuyện với Bepes");
         home.openBepesAI();
     }
 
     public void goToSearch() {
+        logStep("Từ Home → Mở tìm kiếm");
         home.clickSearch();
+    }
+
+    public void openFirstRecipe() {
+        logStep("Từ Home → Mở công thức thịnh hành đầu tiên");
+        home.openFirstRecipe();
+    }
+
+    public void openGoiYTuTuLanh() {
+        logStep("Từ Home → Mở Gợi ý từ tủ lạnh");
+        home.openGoiYTuTuLanh();
     }
 }
