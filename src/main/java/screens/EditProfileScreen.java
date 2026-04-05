f6d0aa45107ae6918e231b8babc0335f902a036a
TuongThiDuyen <dd16102004@gmail.com>
2026-04-06 01:34:34 +0700

    feat: project chefmate-automation - Thực hiện done code Autotest Chinh sua thong tin

--- src/main/java/screens/EditProfileScreen.java
+++ src/main/java/screens/EditProfileScreen.java
@@ -29,71 +29,91 @@ public class EditProfileScreen extends BaseScreen {
     // ===== TOAST ERROR =====
     private final By toastError = By.xpath("//android.widget.Toast[@text='Có lỗi xảy ra, vui lòng thử lại']");
 
+    // ===== TOAST UPDATE INFO THANH CONG ======
+    private final By toastUpdate = By.xpath("//android.widget.Toast[@text='Cập nhật thành công']");
+
     // ===== BACK =====
     private final By btnBack = By.xpath("//android.widget.Button");
+    private final By registerButton = By.xpath("(//android.widget.TextView[@text='Đăng ký'])[2]");
 
     // ================== ACTION ==================
 
     public void openEditProfileScreen() {
         logStep("Mở màn chỉnh sửa thông tin");
 
-        WaitingHelper.waitForClickable(iconEditProfile);
+//        WaitingHelper.waitForClickable(iconEditProfile);
         click(iconEditProfile);
 
 //        WaitingHelper.sleepSeconds(2);
     }
 
-    public boolean isEditProfileDisplayed() {
-        logStep("Verify màn chỉnh sửa thông tin");
-
-        return isDisplayed(titleEditProfile);
-    }
+//    public boolean isEditProfileDisplayed() {
+//        logStep("Verify màn chỉnh sửa thông tin");
+//
+//        return isDisplayed(titleEditProfile);
+//    }
 // ================= INPUT =================
     public void enterFullName(String name) {
-
-        logStep("Nhập họ và tên: " + name);
-
+        logStep("Nhap ho và ten: " + name);
         type(inputFullName, name);
     }
 
     public void enterEmail(String email) {
-
-        logStep("Nhập email: " + email);
-
+        logStep("Nhap email: " + email);
         type(inputEmail, email);
     }
 
     public void enterPhone(String phone) {
-
-        logStep("Nhập số điện thoại: " + phone);
-
+        logStep("Nhap so đien thoai: " + phone);
         type(inputPhone, phone);
     }
 
-
     public void clickSave() {
-        logStep("Click Lưu thay đổi");
-
-        WaitingHelper.waitForClickable(btnSave);
+        logStep("Click Luu thay đoi");
+//        WaitingHelper.waitForClickable(btnSave);
         click(btnSave);
-
-        WaitingHelper.sleepSeconds(6);
+//        WaitingHelper.sleepSeconds(4);
     }
 
     public void clickBack() {
-        logStep("Back về Profile");
-
+        logStep("Back Tai khoan");
         click(btnBack);
         WaitingHelper.sleepSeconds(2);
     }
 
     public boolean isToastErrorDisplayed() {
-        logStep("Verify Toast Error");
+
+        logStep("🔍 Verify Toast: Co loi xay ra, vui long thuc hien lai");
 
         try {
-            return isDisplayed(toastError);
+            Thread.sleep(0000); // chờ toast xuất hiện
+            getDriver().findElement(toastError);
+            logStep("✅ Toast đã hiển thị");
+            return true;
         } catch (Exception e) {
+            logStep("❌ Không thấy Toast");
             return false;
         }
     }
+
+    public boolean isToastUpdateInfoDisplayed() {
+
+        logStep("🔍 Verify Toast: Cap nhat thanh cong");
+
+        try {
+            Thread.sleep(0000); // chờ toast xuất hiện
+            getDriver().findElement(toastUpdate);
+            logStep("✅ Toast đã hiển thị");
+            return true;
+        } catch (Exception e) {
+            logStep("❌ Không thấy Toast");
+            return false;
+        }
+    }
+//    public void clickRegister(){
+//            scrollToText("Đăng ký");
+//            WaitingHelper.waitForClickable(registerButton);
+//            click(registerButton);
+//            logStep("Da click dang ky thanh cong");
+//    }
 }
