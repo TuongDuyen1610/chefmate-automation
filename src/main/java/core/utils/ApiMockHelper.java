6f444fcaaefb7b52996caa03994318d6a560368d
TuongThiDuyen <dd16102004@gmail.com>
2026-04-10 19:06:25 +0700

    feat: project chefmate-automation - Thực hiện Report Auto GoogleSheet & Allure Report DONE
    - Thực hiện Code auto test Doi mat khau

--- src/main/java/core/utils/ApiMockHelper.java
+++
@@ -1,8 +0,0 @@
-package core.utils;
-
-public class ApiMockHelper {
-
-    public static String getMockAIResponse(String question) {
-        return "[Bepes AI Mock] Theo câu hỏi '" + question + "', công thức gợi ý là...";
-    }
-}
