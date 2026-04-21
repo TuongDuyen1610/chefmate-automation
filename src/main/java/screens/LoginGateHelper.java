45b1ee6bddda6a90cc742d4c7450bc981b5b10b6
TuongThiDuyen <dd16102004@gmail.com>
2026-04-22 00:44:17 +0700

    feat: project chefmate-automation - Thực hiện Report Auto GoogleSheet & Allure Report DONE
    - Thực hiện Code auto test TÍNH NĂNG 1: Hiển thị công thức nấu ăn

--- src/main/java/screens/LoginGateHelper.java
+++ src/main/java/screens/LoginGateHelper.java
@@ -14,26 +14,22 @@ public class LoginGateHelper extends BaseScreen {
 
     private final By tabFridge = By.xpath("//android.widget.TextView[@text='Tủ lạnh']");
     private final By tabProfile = By.xpath("//android.widget.TextView[@text='Tài khoản']");
-    private final By btnOpenLoginFormFridge = By.xpath("//android.view.View[2]/android.view.View/android.widget.Button");
+    private final By btnOpenLoginFormFridge = By.xpath("//android.widget.TextView[@text='Đăng nhập']");
     private final By btnOpenLoginFormProfile = By.xpath("//android.widget.TextView[@text='Đăng nhập ngay']");
     public void triggerLoginByFridgeTab() {
         logStep("1. Click vao Tab Tu lanh");
         WaitingHelper.waitForClickable(tabFridge);
         click(tabFridge);
-//        WaitingHelper.sleepSeconds(2);
         WaitingHelper.waitForClickable(btnOpenLoginFormFridge);
         click(btnOpenLoginFormFridge);
-//        WaitingHelper.sleepSeconds(2);
     }
 
     public void triggerLoginByProfileTab() {
         logStep("1. Click vao Tab Tai khoan");
         WaitingHelper.waitForClickable(tabProfile);
         click(tabProfile);
-//        WaitingHelper.sleepSeconds(1);
         WaitingHelper.waitForClickable(btnOpenLoginFormProfile);
         click(btnOpenLoginFormProfile);
-//        WaitingHelper.sleepSeconds(2);
     }
 
 }
