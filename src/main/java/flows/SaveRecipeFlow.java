85aa6cd2b071838a41d61bbb65a660bb9482f304
TuongThiDuyen <dd16102004@gmail.com>
2026-04-24 17:23:24 +0700

    feat: project chefmate-automation - Thực hiện Report Auto GoogleSheet & Allure Report DONE
    - Thực hiện Code Script auto test (Status DONE) cho 2 tính năng:
    + TÍNH NĂNG 4: Lưu vào "Kho công thức"
    + INTERACTION - TÍNH NĂNG 1: Yêu thích (Like)
    + INTERACTION - TÍNH NĂNG 2: Bình luận
    + INTERACTION - TÍNH NĂNG 3: Chia sẻ công thức (ZALO, MESSAGE, GMAIL,.. ) TEST DEVICE REAL

---
+++ src/main/java/flows/SaveRecipeFlow.java
@@ -0,0 +1,42 @@
+package flows;
+
+import screens.ProfileScreen;
+import screens.RecipeDetailScreen;
+import screens.SavedRecipesScreen;
+
+public class SaveRecipeFlow {
+
+    RecipeDetailScreen detail = new RecipeDetailScreen();
+    ProfileScreen profile = new ProfileScreen();
+    SavedRecipesScreen saved = new SavedRecipesScreen();
+
+    public boolean saveAndVerify(String title, String author) {
+
+        // 1. Save
+        boolean savedOk = detail.clickSaveAndVerifyToast();
+
+        // 2. Back về Home
+        detail.clickBackToHome();
+
+        // 3. Mở tab Tài khoản (DÙNG METHOD CÓ SẴN)
+        profile.clickTabProfile();
+
+        // 4. Mở Kho công thức
+        profile.openSavedRecipes();
+
+        // 5. Verify
+        boolean exist = saved.isRecipeExist(title, author);
+        boolean isTop = saved.isNewestOnTop(title);
+
+        return savedOk && exist && isTop;
+    }
+    public int getSavedCount() {
+
+        profile.clickTabProfile();
+        profile.openSavedRecipes();
+        int total = saved.getTotalRecipes();
+        saved.clickBack();
+        profile.clickTabHome();
+        return total;
+    }
+}
