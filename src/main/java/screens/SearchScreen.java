9ff37c1c2c7100083574e8fee8616334a397ce86
TuongThiDuyen <dd16102004@gmail.com>
2026-05-19 16:16:17 +0700

    feat: project chefmate-automation - Tools: Java, Appium, TestNG, (IDE Intellij) - Thực hiện Report Auto GoogleSheet Realtime & Allure Report & Log4j DONE - Update code

--- src/main/java/screens/SearchScreen.java
+++ src/main/java/screens/SearchScreen.java
@@ -160,6 +160,7 @@ public class SearchScreen extends BaseScreen {
 
     @Step("Get result count")
     public int getResultCount() {
+        WaitingHelper.sleepSeconds(0); // đợi UI settle nhẹ
         return getDriver().findElements(resultItems).size();
     }
 
