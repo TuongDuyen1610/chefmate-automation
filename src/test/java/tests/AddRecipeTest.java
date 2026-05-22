8e5492086862951f427e6f9026ab9f1cfa8e4ff5
TuongThiDuyen <dd16102004@gmail.com>
2026-05-22 23:54:20 +0700

    feat: project chefmate-automation - Tools: Java, Appium, TestNG, (IDE Intellij) - Thực hiện Report Auto GoogleSheet Realtime & Allure Report & Log4j DONE
    - UPDATE CODE

--- src/test/java/tests/AddRecipeTest.java
+++ src/test/java/tests/AddRecipeTest.java
@@ -40,209 +40,209 @@ public class AddRecipeTest extends BaseTest {
 //    // =========================
 //    // TC_01 - FORM LOAD
 //    // =========================
-//    @Test(priority = 1)
-//    public void RecipeCreate_TC_01() {
-//
-//        goToAddScreen();
-//
-//        Assert.assertTrue(add.isDisplayed(), "❌ Không vào màn Add");
-//
-//        Assert.assertTrue(
-//                addFlow.verifyFormLoaded(),
-//                "❌ Form không load đúng dữ liệu"
-//        );
-//    }
-//
+    @Test(priority = 1)
+    public void RecipeCreate_TC_01() {
+
+        goToAddScreen();
+
+        Assert.assertTrue(add.isDisplayed(), "❌ Không vào màn Add");
+
+        Assert.assertTrue(
+                addFlow.verifyFormLoaded(),
+                "❌ Form không load đúng dữ liệu"
+        );
+    }
+
 //    // =========================
 //    // TC_02 - UPDATE FULL DATA
 //    // =========================
-//    @Test(priority = 2)
-//    public void RecipeCreate_TC_02() {
-//
-//        goToAddScreen();
-//        // ===== AVATAR =====
-//        add.clickAvatar();
-//        add.chooseImageFromGalleryEmulator(1);
-//
-//        WaitingHelper.sleepSeconds(1);
-//
-//        // ===== TAG =====
-//        List<String> tags = Arrays.asList("Cá", "Kem", "Trứng");
-//
-//        add.addMultipleTags(tags);
-//
-//        // ===== BASIC =====
-//        String name = "Gỏi cuốn " + System.currentTimeMillis();
-//
-//        add.inputName(name);
-//        add.inputTime("30 phút");
-//        add.inputServing("2");
-//
-//        try { getDriver().hideKeyboard(); } catch (Exception ignored) {}
-//
-//        // ===== INGREDIENT =====
-//        List<List<String>> ingredients = Arrays.asList(
-//                Arrays.asList("Bánh tráng", "10", "cái"),
-//                Arrays.asList("Tôm", "200", "gram"),
-//                Arrays.asList("Thịt ba chỉ", "300", "gram")
-////                Arrays.asList("Bún", "500", "gram"),
-////                Arrays.asList("Rau sống", "1", "bó"),
-////                Arrays.asList("Nước mắm", "50", "ml")
-//        );
-//
-//        add.inputIngredients(ingredients);
-//        try { getDriver().hideKeyboard(); } catch (Exception ignored) {}
-//
-//        // ===== STEP =====
-//        List<String> steps = Arrays.asList(
-//                "Luộc tôm và thịt",
-//                "Chuẩn bị rau và bún",
-//                "Nhúng bánh tráng",
-//                "Xếp nguyên liệu"
+    @Test(priority = 2)
+    public void RecipeCreate_TC_02() {
 
-    /// /                "Cuốn chặt tay",
-    /// /                "Pha nước chấm"
-//        );
-//        add.inputSteps(steps);
-//
-//        // ===== POST =====
-//        add.clickPost();
-//
-//        // ===== VERIFY =====
-//        profile.clickTabProfile();
-//        profile.openSavedRecipes();
-//        Assert.assertTrue(
-//                saved.isRecipeExist(name, ""),
-//                "❌ Không add thành công"
-//        );
-//    }
+        goToAddScreen();
+        // ===== AVATAR =====
+        add.clickAvatar();
+        add.chooseImageFromGalleryEmulator(1);
+
+        WaitingHelper.sleepSeconds(1);
+
+        // ===== TAG =====
+        List<String> tags = Arrays.asList("Cá", "Kem", "Trứng");
+
+        add.addMultipleTags(tags);
+
+        // ===== BASIC =====
+        String name = "Gỏi cuốn " + System.currentTimeMillis();
+
+        add.inputName(name);
+        add.inputTime("30 phút");
+        add.inputServing("2");
+
+        try { getDriver().hideKeyboard(); } catch (Exception ignored) {}
+
+        // ===== INGREDIENT =====
+        List<List<String>> ingredients = Arrays.asList(
+                Arrays.asList("Bánh tráng", "10", "cái"),
+                Arrays.asList("Tôm", "200", "gram"),
+                Arrays.asList("Thịt ba chỉ", "300", "gram")
+//                Arrays.asList("Bún", "500", "gram"),
+//                Arrays.asList("Rau sống", "1", "bó"),
+//                Arrays.asList("Nước mắm", "50", "ml")
+        );
+
+        add.inputIngredients(ingredients);
+        try { getDriver().hideKeyboard(); } catch (Exception ignored) {}
+
+        // ===== STEP =====
+        List<String> steps = Arrays.asList(
+                "Luộc tôm và thịt",
+                "Chuẩn bị rau và bún",
+                "Nhúng bánh tráng",
+                "Xếp nguyên liệu"
+
+    //                "Cuốn chặt tay",
+    //                "Pha nước chấm"
+        );
+        add.inputSteps(steps);
+
+        // ===== POST =====
+        add.clickPost();
+
+        // ===== VERIFY =====
+        profile.clickTabProfile();
+        profile.openSavedRecipes();
+        Assert.assertTrue(
+                saved.isRecipeExist(name, ""),
+                "❌ Không add thành công"
+        );
+    }
     // =========================
     // TC_03 - REQUIRED FIELD
     // =========================
-//    @Test(priority = 3)
-//    public void RecipeCreate_TC_03() {
-//
-//        goToAddScreen();
-//
-//        addFlow.clearRequiredField();
-//
-//        // 🔥 ĐẢM BẢO KHÔNG BỊ KEYBOARD CHẶN
-//        try {
-//            getDriver().hideKeyboard();
-//        } catch (Exception ignored) {}
-//
-//        add.slowSwipeDownOnScreen(2);
-//
-//        add.clickPost(); // ✅ FIX
-//
-//        Assert.assertTrue(
-//                addFlow.isRequiredToast(),
-//                "❌ Không hiển thị toast required"
-//        );
-//    }
+    @Test(priority = 3)
+    public void RecipeCreate_TC_03() {
+
+        goToAddScreen();
+
+        addFlow.clearRequiredField();
+
+        // 🔥 ĐẢM BẢO KHÔNG BỊ KEYBOARD CHẶN
+        try {
+            getDriver().hideKeyboard();
+        } catch (Exception ignored) {}
+
+        add.slowSwipeDownOnScreen(2);
+
+        add.clickPost(); // ✅ FIX
+
+        Assert.assertTrue(
+                addFlow.isRequiredToast(),
+                "❌ Không hiển thị toast required"
+        );
+    }
     // =========================
     // TC_04 - EMPTY INGREDIENT
     // =========================
-//        @Test(priority = 4)
-//        public void RecipeCreate_TC_04() {
-//
-//        goToAddScreen();
-//        // ===== AVATAR =====
-//        add.clickAvatar();
-//        add.chooseImageFromGalleryEmulator(1);
-//
-//        WaitingHelper.sleepSeconds(1);
-//        // ===== TAG =====
-//        List<String> tags = Arrays.asList("Cá");
-//
-//        add.addMultipleTags(tags);
-//        // ===== BASIC =====
-//        String name = "Gỏi cuốn " + System.currentTimeMillis();
-//
-//        add.inputName(name);
-//        add.inputTime("30 phút");
-//        add.inputServing("2");
-//
-//        try { getDriver().hideKeyboard(); } catch (Exception ignored) {}
-//        add.slowSwipeDownOnScreen(2);
-//        // ===== INGREDIENT =====
-//        List<List<String>> ingredients = Arrays.asList(
-//                Arrays.asList("", "", ""),
-//                Arrays.asList("", "", ""),
-//                Arrays.asList("", "", "")
-//        );
-//        add.inputIngredients(ingredients);
-//        try { getDriver().hideKeyboard(); } catch (Exception ignored) {}
-//        // ===== STEP =====
-//        List<String> steps = Arrays.asList(
-//                "Luộc tôm và thịt",
-//                "Chuẩn bị rau và bún",
-//                "Nhúng bánh tráng",
-//                "Xếp nguyên liệu"
-//        );
-//        add.inputSteps(steps);
-//
-//        // ===== POST =====
-//        add.clickPost();
-//
-//        Assert.assertTrue(
-//                addFlow.isInvalidToast(),
-//                "❌ Update ingredient lỗi"
-//        );
-//    }
+        @Test(priority = 4)
+        public void RecipeCreate_TC_04() {
+
+        goToAddScreen();
+        // ===== AVATAR =====
+        add.clickAvatar();
+        add.chooseImageFromGalleryEmulator(1);
+
+        WaitingHelper.sleepSeconds(1);
+        // ===== TAG =====
+        List<String> tags = Arrays.asList("Cá");
+
+        add.addMultipleTags(tags);
+        // ===== BASIC =====
+        String name = "Gỏi cuốn " + System.currentTimeMillis();
+
+        add.inputName(name);
+        add.inputTime("30 phút");
+        add.inputServing("2");
+
+        try { getDriver().hideKeyboard(); } catch (Exception ignored) {}
+        add.slowSwipeDownOnScreen(2);
+        // ===== INGREDIENT =====
+        List<List<String>> ingredients = Arrays.asList(
+                Arrays.asList("", "", ""),
+                Arrays.asList("", "", ""),
+                Arrays.asList("", "", "")
+        );
+        add.inputIngredients(ingredients);
+        try { getDriver().hideKeyboard(); } catch (Exception ignored) {}
+        // ===== STEP =====
+        List<String> steps = Arrays.asList(
+                "Luộc tôm và thịt",
+                "Chuẩn bị rau và bún",
+                "Nhúng bánh tráng",
+                "Xếp nguyên liệu"
+        );
+        add.inputSteps(steps);
+
+        // ===== POST =====
+        add.clickPost();
+
+        Assert.assertTrue(
+                addFlow.isInvalidToast(),
+                "❌ Update ingredient lỗi"
+        );
+    }
     // =========================
     // TC_05 - EMPTY STEP
     // =========================
-//    @Test(priority = 5)
-//    public void RecipeCreate_TC_05() {
-//        goToAddScreen();
-//        // ===== AVATAR =====
-//        add.clickAvatar();
-//        add.chooseImageFromGalleryEmulator(1);
-//
-//        WaitingHelper.sleepSeconds(1);
-//        // ===== TAG =====
-//        List<String> tags = Arrays.asList("Cá");
-//
-//        add.addMultipleTags(tags);
-//        // ===== BASIC =====
-//        String name = "Gỏi cuốn " + System.currentTimeMillis();
-//
-//        add.inputName(name);
-//        add.inputTime("30 phút");
-//        add.inputServing("2");
-//
-//        try { getDriver().hideKeyboard(); } catch (Exception ignored) {}
-//        add.slowSwipeDownOnScreen(2);
-//        // ===== INGREDIENT =====
-//        List<List<String>> ingredients = Arrays.asList(
-//                Arrays.asList("Bánh tráng", "10", "cái"),
-//                Arrays.asList("Tôm", "200", "gram"),
-//                Arrays.asList("Thịt ba chỉ", "300", "gram"));
-//
-//        add.inputIngredients(ingredients);
-//        try { getDriver().hideKeyboard(); } catch (Exception ignored) {}
-//        // ===== STEP =====
-//        List<String> steps = Arrays.asList(
-//                "",
-//                "",
-//                "",
-//                ""
-//        );
-//        add.inputSteps(steps);
-//
-//        // ===== POST =====
-//        add.clickPost();
-//
-//        Assert.assertTrue(
-//                addFlow.isRequiredToast(),
-//                "❌ Update steps lỗi"
-//        );
-//    }
+    @Test(priority = 5)
+    public void RecipeCreate_TC_05() {
+        goToAddScreen();
+        // ===== AVATAR =====
+        add.clickAvatar();
+        add.chooseImageFromGalleryEmulator(1);
+
+        WaitingHelper.sleepSeconds(1);
+        // ===== TAG =====
+        List<String> tags = Arrays.asList("Cá");
+
+        add.addMultipleTags(tags);
+        // ===== BASIC =====
+        String name = "Gỏi cuốn " + System.currentTimeMillis();
+
+        add.inputName(name);
+        add.inputTime("30 phút");
+        add.inputServing("2");
+
+        try { getDriver().hideKeyboard(); } catch (Exception ignored) {}
+        add.slowSwipeDownOnScreen(2);
+        // ===== INGREDIENT =====
+        List<List<String>> ingredients = Arrays.asList(
+                Arrays.asList("Bánh tráng", "10", "cái"),
+                Arrays.asList("Tôm", "200", "gram"),
+                Arrays.asList("Thịt ba chỉ", "300", "gram"));
+
+        add.inputIngredients(ingredients);
+        try { getDriver().hideKeyboard(); } catch (Exception ignored) {}
+        // ===== STEP =====
+        List<String> steps = Arrays.asList(
+                "",
+                "",
+                "",
+                ""
+        );
+        add.inputSteps(steps);
+
+        // ===== POST =====
+        add.clickPost();
+
+        Assert.assertTrue(
+                addFlow.isRequiredToast(),
+                "❌ Update steps lỗi"
+        );
+    }
 
     // =========================
-//    // TC_06 - UPDATE FULL DATA STATUS NOT LOG IN
-//    // =========================
+    // TC_06 - UPDATE FULL DATA STATUS NOT LOG IN
+    // =========================
     @Test(priority = 6)
     public void RecipeCreate_TC_06() {
         add.clickScreenAddRecipe();//
