73d70fd48622d36680f36ef4effaa69cd0dbfbf2
TuongThiDuyen <dd16102004@gmail.com>
2026-05-22 23:55:14 +0700

    feat: project chefmate-automation - Tools: Java, Appium, TestNG, (IDE Intellij) - Thực hiện Report Auto GoogleSheet Realtime & Allure Report & Log4j DONE
    - UPDATE CODE

--- src/test/java/tests/FridgeTest.java
+++ src/test/java/tests/FridgeTest.java
@@ -1,19 +1,19 @@
-package tests;
-
-import core.base.BaseTest;
-import flows.FridgeFlow;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
-public class FridgeTest extends BaseTest {
-
-    private final FridgeFlow fridge = new FridgeFlow();
-
-    @Test(priority = 1, description = "Smoke 07 - Thêm nguyên liệu vào tủ lạnh")
-    public void TC07_Fridge_Add_Ingredient_Success() {
-        fridge.openFridge();
-        fridge.addIngredient("Trứng gà", "2", "quả");
-        Assert.assertTrue(true, "Đã thêm nguyên liệu thành công");
-        System.out.println("✅ TC07 PASS - Fridge OK");
-    }
-}
+//package tests;
+//
+//import core.base.BaseTest;
+//import flows.FridgeFlow;
+//import org.testng.Assert;
+//import org.testng.annotations.Test;
+//
+//public class FridgeTest extends BaseTest {
+//
+//    private final FridgeFlow fridge = new FridgeFlow();
+//
+//    @Test(priority = 1, description = "Smoke 07 - Thêm nguyên liệu vào tủ lạnh")
+//    public void TC07_Fridge_Add_Ingredient_Success() {
+//        fridge.openFridge();
+//        fridge.addIngredient("Trứng gà", "2", "quả");
+//        Assert.assertTrue(true, "Đã thêm nguyên liệu thành công");
+//        System.out.println("✅ TC07 PASS - Fridge OK");
+//    }
+//}
