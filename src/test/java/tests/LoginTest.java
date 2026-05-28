108c22af0ea7dff35fc27daa24c28e1114794982
TuongThiDuyen <dd16102004@gmail.com>
2026-05-28 21:44:19 +0700

    feat: project chefmate-automation - Tools: Java, Appium, TestNG, (IDE Intellij) - Thực hiện Report Auto GoogleSheet Realtime & Allure Report & Log4j DONE
    - UPDATE CODE

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -9,6 +9,9 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import screens.LoginGateHelper;
+import screens.LoginScreen;
+import screens.SplashScreen;
 
 
 @Feature("Authentication")
@@ -16,7 +19,9 @@ public class LoginTest extends BaseTest {
     private static final Logger logger = LoggerFactory.getLogger(LoginTest.class);
     private final AuthenticationFlow authFlow = new AuthenticationFlow();
     private final LoginData loginData = JsonHelper.readLoginData();
-
+    private final LoginScreen login = new LoginScreen();
+    private final SplashScreen splash = new SplashScreen();
+    private final LoginGateHelper gateHelper = new LoginGateHelper();
     String phoneOrEmail = loginData.login;
     String password = loginData.password;
 
@@ -54,32 +59,116 @@ public class LoginTest extends BaseTest {
         Assert.assertTrue(authFlow.isLoggedInSuccessfully(), "Error: Khong vao đuoc Home!");
         logger.info("✅ DangNhap_TC_04 PASSED\n");
     }
+
+    @Test(priority = 5, description = "DangNhap_TC_05 - Đăng nhập Tài khoản")
+    public void DangNhap_TC_05() {
+        authFlow.loginFromProfileTab(phoneOrEmail, password);
+        AllureHelper.attachScreenshot("Login successful");
+        Assert.assertTrue(authFlow.isLoggedInSuccessfully(), "Error: Khong vao đuoc Home!");
+        logger.info("✅ DangNhap_TC_05 PASSED");
+
+    }
+
+    @Test(priority = 6, description = "DangNhap_TC_06 - Đăng nhập Tài khoản")
+    public void DangNhap_TC_06() {
+        String phoneOrEmail = "0900009999";
+        authFlow.loginFromProfileTab(phoneOrEmail, password);
+        AllureHelper.attachScreenshot("Login successful");
+        Assert.assertTrue(authFlow.isLoggedInSuccessfully(), "Error: Khong vao đuoc Home!");
+        logger.info("✅ DangNhap_TC_06 PASSED");
+
+    }
     // ==================== UNHAPPY CASE ====================
 
-    @Test(priority = 5, description = "DangNhap_TC_05 - Tài khoản/mật khẩu không hợp lệ")
-    public void DangNhap_TC_05() {
+    @Test(priority = 7, description = "DangNhap_TC_07 - Tài khoản/mật khẩu không hợp lệ")
+    public void DangNhap_TC_07() {
         String phoneOrEmail = "wrongaccount@gmail.com";
         String password = "wrongpassword";
         authFlow.loginFromFridgeTab(phoneOrEmail, password);
         AllureHelper.attachScreenshot("Login fail");
         Assert.assertTrue(authFlow.isLoginScreenStillDisplayed(), "Error: Phai o lai man Login!");
-        logger.info("✅ DangNhap_TC_05 PASSED\n");
+        logger.info("✅ DangNhap_TC_07 PASSED\n");
     }
 
-    @Test(priority = 6, description = "DangNhap_TC_06- Để trống Email")
-    public void DangNhap_TC_06() {
+    @Test(priority = 8, description = "DangNhap_TC_08- Để trống Email")
+    public void DangNhap_TC_08() {
         String password = "000000";
         authFlow.TC_06(password);
         Assert.assertTrue(authFlow.isToastUpdateInfoDisplayed_TC0607(), "Error: Toast error khong hien thi");
-        logger.info("✅ DangNhap_TC_06 PASSED\n");
+        logger.info("✅ DangNhap_TC_08 PASSED\n");
     }
 
-    @Test(priority = 7, description = "DangNhap_TC_07 - Để trống Mật khẩu")
-    public void DangNhap_TC_07() {
+    @Test(priority = 9, description = "DangNhap_TC_09 - Để trống Mật khẩu")
+    public void DangNhap_TC_09() {
         String phoneOrEmail = "0000000000";
         authFlow.TC_07(phoneOrEmail);
         Assert.assertTrue(authFlow.isToastUpdateInfoDisplayed_TC0607(), "Loi: Toast error khong hien thi");
-        logger.info("✅ DangNhap_TC_07 PASSED\n");
+        logger.info("✅ DangNhap_TC_09 PASSED\n");
 
     }
+
+    @Test(priority = 10, description = "DangNhap_TC_10 - Để trống cả 2 trường")
+    public void DangNhap_TC_10() {
+        splash.waitUntilSplashDisappear();
+        AllureHelper.attachScreenshot("Splash screen disappeared");
+        gateHelper.triggerLoginByFridgeTab();
+        login.clickLogin();
+        Assert.assertTrue(authFlow.isToastUpdateInfoDisplayed_TC0607(), "Loi: Toast error khong hien thi");
+        logger.info("✅ DangNhap_TC_10 PASSED\n");
+
+    }
+
+    @Test(priority = 11, description = "DangNhap_TC_11 - Email sai định dạng")
+    public void DangNhap_TC_11() {
+        authFlow.loginFromFridgeTab("wrongaccount.com", "123456");
+        AllureHelper.attachScreenshot("Login fail");
+        Assert.assertTrue(authFlow.isToastLoginFail(), "Loi: Toast error khong hien thi");
+        logger.info("✅ DangNhap_TC_11 PASSED\n");
+    }
+    @Test(priority = 12, description = "DangNhap_TC_12 - SĐT sai định dạng")
+    public void DangNhap_TC_12() {
+        authFlow.loginFromFridgeTab("1234abc", "123456");
+        AllureHelper.attachScreenshot("Login fail");
+        Assert.assertTrue(authFlow.isToastLoginFail(), "Loi: Toast error khong hien thi");
+        logger.info("✅ DangNhap_TC_12 PASSED\n");
+    }
+    @Test(priority = 13, description = "DangNhap_TC_13 - field có dấu cách/trước/sau")
+    public void DangNhap_TC_13() {
+        authFlow.loginFromFridgeTab(" duyentest@gmail.com ", " 123456 ");
+        AllureHelper.attachScreenshot("Login fail");
+        Assert.assertTrue(authFlow.isLoggedInSuccessfully(), "Error: Khong vao đuoc Home!");
+        logger.info("✅ DangNhap_TC_13 PASSED\n");
+    }
+    @Test(priority = 14, description = "DangNhap_TC_14 - field có dấu tiếng Việt, ký tự đặc biệt")
+    public void DangNhap_TC_14() {
+        authFlow.loginFromFridgeTab("duyêntest@gmail.com", "abc@#%$%^%&*&%"); // dữ liệu dị biệt
+        AllureHelper.attachScreenshot("Login fail");
+        Assert.assertTrue(authFlow.isToastLoginFail(), "Loi: Toast error khong hien thi");
+        logger.info("✅ DangNhap_TC_14 PASSED\n");
+    }
+    @Test(priority = 15, description = "DangNhap_TC_15 -  Email, mật khẩu max ký tự quy định")
+    public void DangNhap_TC_15() {
+        String longEmail = "a".repeat(245) + "@gmail.com"; // > max cho phép
+        String longPassword = "a".repeat(100);             // password dài bất thường
+        authFlow.loginFromFridgeTab(longEmail, longPassword);
+        AllureHelper.attachScreenshot("Login fail");
+        Assert.assertTrue(authFlow.isToastLoginFail(), "Loi: Toast error khong hien thi");
+        logger.info("✅ DangNhap_TC_15 PASSED\n");
+    }
+    @Test(priority = 16, description = "DangNhap_TC_16 -  icon ẩn/ hiển password")
+    public void DangNhap_TC_16() {
+        splash.waitUntilSplashDisappear();
+        AllureHelper.attachScreenshot("Splash screen disappeared");
+        gateHelper.triggerLoginByFridgeTab();
+        login.performLogin2("0900009999", "123456");
+        logger.info("✅ DangNhap_TC_16 PASSED");
+    }
+    @Test(priority = 17, description = "DangNhap_TC_17 -  toàn chữ hoa/thường")
+    public void DangNhap_TC_17() {
+        authFlow.loginFromFridgeTab("DUYENTEST@GMAIL.COM", "123456");
+        AllureHelper.attachScreenshot("Login fail");
+        Assert.assertTrue(authFlow.isLoggedInSuccessfully(), "Error: Khong vao đuoc Home!");
+        logger.info("✅ DangNhap_TC_17 PASSED\n");
+    }
+
 }
