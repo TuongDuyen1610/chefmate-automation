6ca0d8d982ee123893e46ee81e93cf8a799df3be
TuongThiDuyen <dd16102004@gmail.com>
2026-06-05 21:28:26 +0700

    feat: project chefmate-automation - Tools: Java, Appium, TestNG, (IDE Intellij) - Thực hiện Report Auto GoogleSheet Realtime & Allure Report & Log4j DONE
    - CI/CD Tích hợp GitHub Actions chạy tự động luồng E2E

--- src/main/java/screens/RecipeDetailScreen.java
+++ src/main/java/screens/RecipeDetailScreen.java
@@ -526,7 +526,8 @@ public class RecipeDetailScreen extends BaseScreen {
 
             // click lần 2 để trigger toast
             click(btnSave);
-
+            click(btnSave);
+            click(btnSave);
             // 🔥 QUAN TRỌNG: delay NGẮN để toast kịp render
             WaitingHelper.sleepSeconds(1);
 
