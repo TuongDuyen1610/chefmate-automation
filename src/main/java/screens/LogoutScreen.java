5c160ded2df6c9266e056762badab32d4ce5884d
TuongThiDuyen <dd16102004@gmail.com>
2026-06-23 22:30:56 +0700

    feat: project chefmate-automation - Tools: Java, Appium, TestNG, (IDE Intellij) - Thực hiện Report Auto GoogleSheet Realtime & Allure Report & Log4j DONE
    - CI/CD Tích hợp GitHub Actions chạy tự động luồng E2E

--- src/main/java/screens/LogoutScreen.java
+++ src/main/java/screens/LogoutScreen.java
@@ -1,8 +1,11 @@
 package screens;
 
 import core.base.BaseScreen;
+import core.utils.AllureHelper;
 import core.utils.WaitingHelper;
 import org.openqa.selenium.By;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * LogoutScreen.java
@@ -10,6 +13,7 @@ import org.openqa.selenium.By;
  * Locators từ chị cung cấp
  */
 public class LogoutScreen extends BaseScreen {
+    private static final Logger logger = LoggerFactory.getLogger(ProfileScreen.class);
 
     // ==================== LOGOUT ICON LOCATORS ====================
     // Nút icon logout biểu tượng trên trang cá nhân tại tab tài khoản
@@ -24,8 +28,8 @@ public class LogoutScreen extends BaseScreen {
     // ==================== CANCEL BUTTON LOCATORS ====================
     // Nút "X" để hủy đăng xuất tài khoản
 //    private final By cancelButtonX = By.xpath("//android.widget.Button1");
-//    private final By cancelButtonX = By.xpath("//android.view.View[@content-desc='Close sheet']");
-    private final By cancelButtonX = By.xpath("//android.view.View[@content-desc='Đóng trang tính']");
+    private final By cancelButtonX = By.xpath("//android.view.View[@content-desc='Close sheet']");//PC
+//    private final By cancelButtonX = By.xpath("//android.view.View[@content-desc='Đóng trang tính']");//LAPTOP
     private final By cancelButtonImageView = By.xpath("//android.widget.ImageView[@content-desc='Account Logout']");
 
     // ==================== CONFIRM LOGOUT LOCATORS ====================
@@ -36,6 +40,8 @@ public class LogoutScreen extends BaseScreen {
     // Nút đăng xuất cuối cùng
     private final By confirmLogoutButton = By.xpath("//android.widget.TextView[@text='Đăng xuất']");
     private final By confirmLogoutButtonAlt = By.xpath("//android.view.ViewGroup/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View[2]");
+
+    private final By toastIsLogoutNotLogin = By.xpath("//android.widget.Toast[@text='Vui lòng đăng nhập để sử dụng tính năng này']");
 
     // ==================== ACTIONS ====================
 
@@ -90,6 +96,22 @@ public class LogoutScreen extends BaseScreen {
             cancelLogout();
         } else {
             logStep("❌ Popup Logout khong hien thi!");
+        }
+    }
+    public boolean isLogoutNotLogin(){
+        logStep("🔍 Verify Toast: Vui long đien đay đu thong tin");
+        click(logoutIcon);
+        WaitingHelper.sleepSeconds(1);
+        try {
+            getDriver().findElement(toastIsLogoutNotLogin);
+            logger.info("✅ Toast error msg hien thi thanh cong");
+            AllureHelper.attachScreenshot("Toast error displayed");
+            logStep("✅ Toast displayed");
+            return true;
+        } catch (Exception e) {
+            logger.info("✅ Toast error msg khong hien thi ");
+            AllureHelper.attachScreenshot("Toast error not displayed");
+            return false;
         }
     }
 }
