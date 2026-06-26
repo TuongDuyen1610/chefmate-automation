59ebecb254f76998dab7204c0039412576723b7e
TuongThiDuyen <dd16102004@gmail.com>
2026-06-26 14:52:06 +0700

    feat: project chefmate-automation - Tools: Java, Appium, TestNG, (IDE Intellij) - Thực hiện Report Auto GoogleSheet Realtime & Allure Report & Log4j DONE
    - CI/CD Tích hợp GitHub Actions chạy tự động luồng E2E

--- src/test/java/tests/RegistrationTest.java
+++ src/test/java/tests/RegistrationTest.java
@@ -290,11 +290,11 @@ public class RegistrationTest extends BaseTest {
 //    }
         @Test(priority = 1, enabled = true, description = "DangKy_TC_01 - Đăng ký thành công")
             public void DangKy_TC_01() {
-
-                String fullName = "Tuong Thi Duyen02";
-                String phone = "0900008888";
-                String email = "duyen@gmail.com";
-                String password = "123456";
+                String ts = String.valueOf(System.currentTimeMillis()).substring(2);
+                String fullName = "Tuong Thi Duyen" + ts;
+                String phone = "09000088" + ts;
+                String email = "duyentest" + ts + "@gmail.com";
+                String password = "123456" + ts;
 
                 authFlow.registerNewAccount(fullName, phone, email, password, password);
                 Assert.assertTrue(
